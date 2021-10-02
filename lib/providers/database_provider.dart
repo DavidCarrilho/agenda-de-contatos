d29d1f4e74217a3805c0da552a9ad4d5e788f78f
DavidCarrilho <luan.david.oliver@gmail.com>
2021-10-02 02:22:52 -0300

    create fields columns

--- lib/providers/database_provider.dart
+++ lib/providers/database_provider.dart
@@ -1,10 +1,18 @@
+// Nome dos campos na tabela do banco de dados, por isso são constantes
+final String contactTable = "contactTable";
+final String idColun = "idColumn";
+final String nameColumn = "nameColumn";
+final String emailColumn = "emailColumn";
+final String phoneColumn = "phoneColumn";
 
 ///A classe DatabaseProvider cuidará especificamente
 // das operações diretas ao banco de dados (criar, editar, remover,
 // abrir e fechar conexão)
-class DatabaseProvider{}
+class DatabaseProvider {}
 
 // Contact focará em representar a estrutura de dados do contato, estrutura tal que a
-// DatabaseProvider utilizará para salvar no banco de dados as informações que forem 
+// DatabaseProvider utilizará para salvar no banco de dados as informações que forem
 //cadastradas, editadas ou removidas pelo usuário
-class Contact{}
+class Contact {
+
+}
