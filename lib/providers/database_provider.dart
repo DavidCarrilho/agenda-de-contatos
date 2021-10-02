d3f2fcd2d79f80db3b4fdb536168b80516223ef7
DavidCarrilho <luan.david.oliver@gmail.com>
2021-10-02 02:19:15 -0300

    create the classes

---
+++ lib/providers/database_provider.dart
@@ -0,0 +1,10 @@
+
+///A classe DatabaseProvider cuidará especificamente
+// das operações diretas ao banco de dados (criar, editar, remover,
+// abrir e fechar conexão)
+class DatabaseProvider{}
+
+// Contact focará em representar a estrutura de dados do contato, estrutura tal que a
+// DatabaseProvider utilizará para salvar no banco de dados as informações que forem 
+//cadastradas, editadas ou removidas pelo usuário
+class Contact{}
