46368e5bbb00f926c83f9fdfd6c055f61d48c34a
DavidCarrilho <luan.david.oliver@gmail.com>
2021-10-02 22:26:29 -0300

    create method _getAllContacts in homepage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,32 @@
+import 'package:app_contact_book/model/model.dart';
+import 'package:app_contact_book/providers/providers.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  DatabaseProvider database = DatabaseProvider();
+  List<Contact> contacts = List();
+
+  @override
+  void initState() {
+    super.initState();
+    _getAllContacts();
+  }
+
+  void _getAllContacts() {
+    database.getAllContacts().then((list) {
+      setState(() {
+        contacts = list;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      
-    );
+    return Scaffold();
   }
 }
