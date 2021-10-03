0ffb44f1d1d363bc64f15963a62fdaa502d1a308
DavidCarrilho <luan.david.oliver@gmail.com>
2021-10-02 23:45:40 -0300

    create values of ContactPage

--- lib/pages/contact_page.dart
+++ lib/pages/contact_page.dart
@@ -1,11 +1,24 @@
+import 'package:app_contact_book/model/model.dart';
 import 'package:flutter/material.dart';
 
-class ContactPage extends StatelessWidget {
+class ContactPage extends StatefulWidget {
+  final Contact contact;
+
+  ContactPage({this.contact});
+
+  @override
+  _ContactPageState createState() => _ContactPageState();
+}
+
+class _ContactPageState extends State<ContactPage> {
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _phoneController = TextEditingController();
+  final _nameFocus = FocusNode();
+  Contact _editedContact;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return Container();
   }
 }
